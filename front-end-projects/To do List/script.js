f73f9a4ed2e12888a77a0b8a5204be89e26ebfe9
Samet Yıldırım <smtyldrm32@gmail.com>
2023-02-27 03:19:45 +0300

    responsive

--- front-end-projects/To do List/script.js
+++ front-end-projects/To do List/script.js
@@ -1,34 +1,34 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("li");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-}
+// // Create a "close" button and append it to each list item
+// var myNodelist = document.getElementsByTagName("li");
+// var i;
+// for (i = 0; i < myNodelist.length; i++) {
+//     var span = document.createElement("SPAN");
+//     var txt = document.createTextNode("\u00D7");
+//     span.className = "close";
+//     span.appendChild(txt);
+//     myNodelist[i].appendChild(span);
+// }
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
-}
+// // Click on a close button to hide the current list item
+// var close = document.getElementsByClassName("close");
+// var i;
+// for (i = 0; i < close.length; i++) {
+//     close[i].onclick = function () {
+//         var div = this.parentElement;
+//         div.style.display = "none";
+//     }
+// }
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function (ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
+// // Add a "checked" symbol when clicking on a list item
+// var list = document.querySelector('ul');
+// list.addEventListener('click', function (ev) {
+//     if (ev.target.tagName === 'LI') {
+//         ev.target.classList.toggle('checked');
+//     }
+// }, false);
 
 function alertClose() {
-    document.getElementById("alert").style.opacity = 0;
+    document.getElementById("alert").style.display = "none";
 }
 
 // Create a new list item when clicking on the "Add" button
@@ -40,8 +40,8 @@ function yeniElement() {
     var alert = document.getElementById("alert")
     li.appendChild(t);
     if (inputValue === '') {
-        alert.style.opacity = 1;
-        alert("Bi şeyler yazmalısın dostum!");
+        alert.style.display = "flex";
+        // alert("Bi şeyler yazmalısın dostum!");
     } else {
         document.getElementById("myWork").appendChild(li);
     }
