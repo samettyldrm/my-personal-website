44243637abae6f6c54e75c597ecbd49d3231b55c
Samet Yıldırım <106842297+samettyldrm@users.noreply.github.com>
2023-03-03 22:36:55 +0300

    Update script.js

--- front-end-projects/toDoList/script.js
+++ front-end-projects/toDoList/script.js
@@ -62,7 +62,7 @@ function createItem(){
 }
 
 //yeniElement() fonksiyonu
-var inputError = document.querySelector("container)
+var inputError = document.querySelector("container")
 //var inputError = document.getElementById("myInput")
 inputError.style.pointerEvents= "auto"
 inputError.style.userSelect= "auto"
